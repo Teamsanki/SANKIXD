7eb82a745ce29574bcd651b4515a37707ed71d1d
𝐓𝐄𝐀𝐌 𝐒𝐀𝐍𝐊𝐈 <teamsanki001@gmail.com>
2024-08-22 14:28:19 +0530

    Add files via upload

---
+++ SANKIXD/plugins/tools/ping.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+
+from pyrogram import filters
+from pyrogram.types import Message
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto
+from config import *
+from SANKIXD import app
+from SANKIXD.core.call import SANKI
+from SANKIXD.utils import bot_sys_stats
+from SANKIXD.utils.decorators.language import language
+from SANKIXD.utils.inline import supp_markup
+from config import BANNED_USERS
+
+
+@app.on_message(filters.command("ping", prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & ~BANNED_USERS)
+@language
+async def ping_com(client, message: Message, _):
+    start = datetime.now()
+    response = await message.reply_video(
+        video="https://graph.org/file/5690109178f081adf464d.mp4",
+        caption=_["ping_1"].format(app.mention),
+    )
+    pytgping = await SANKI.ping()
+    UP, CPU, RAM, DISK = await bot_sys_stats()
+    resp = (datetime.now() - start).microseconds / 1000
+    await response.edit_text(
+        _["ping_2"].format(resp, app.mention, UP, RAM, CPU, DISK, pytgping),
+        reply_markup=supp_markup(_),
+    )
