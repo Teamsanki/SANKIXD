6389dffeaaad675817f8cd30c3eeb9356a90c8c0
𝐓𝐄𝐀𝐌 𝐒𝐀𝐍𝐊𝐈 <teamsanki001@gmail.com>
2024-11-04 01:02:14 +0530

    Update ping.py

--- SANKIXD/plugins/tools/ping.py
+++ SANKIXD/plugins/tools/ping.py
@@ -17,7 +17,7 @@ from config import BANNED_USERS
 async def ping_com(client, message: Message, _):
     start = datetime.now()
     response = await message.reply_video(
-        video="https://graph.org/file/5690109178f081adf464d.mp4",
+        video="https://telegra.ph/file/5a9a513482f4236022cea.mp4",
         caption=_["ping_1"].format(app.mention),
     )
     pytgping = await SANKI.ping()
