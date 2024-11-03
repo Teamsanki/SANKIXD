3b480515747abe981054d4c4c5b494f9b3643f34
𝐓𝐄𝐀𝐌 𝐒𝐀𝐍𝐊𝐈 <teamsanki001@gmail.com>
2024-11-03 17:23:23 +0530

    Update logger.py

--- SANKIXD/utils/logger.py
+++ SANKIXD/utils/logger.py
@@ -10,15 +10,15 @@ async def play_logs(message, streamtype):
         logger_text = f"""
 <b>{app.mention} ᴘʟᴀʏ ʟᴏɢ</b>
 
-<b>ᴄʜᴀᴛ ɪᴅ :</b> <code>{message.chat.id}</code>
-<b>ᴄʜᴀᴛ ɴᴀᴍᴇ :</b> {message.chat.title}
-<b>ᴄʜᴀᴛ ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.chat.username}
+<b>⦿ 𝐂𝐡𝐚𝐭 𝐈𝐃 ➺ :</b> <code>{message.chat.id}</code>
+<b>⦿ 𝐂𝐡𝐚𝐭 𝐍𝐚𝐦𝐞 ➺ :</b> {message.chat.title}
+<b>⦿ 𝐂𝐡𝐚𝐭 𝐋𝐢𝐧𝐤 ➺ :</b> @{message.chat.username}
 
-<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>
-<b>ɴᴀᴍᴇ :</b> {message.from_user.mention}
-<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}
+<b>⦿ 𝐈𝐝 ➺ :</b> <code>{message.from_user.id}</code>
+<b>⦿ 𝐔𝐬𝐞𝐫 ➺ :</b> {message.from_user.mention}
+<b>⦿ 𝐔𝐬𝐞𝐫𝐧𝐚𝐦𝐞 ➺ :</b> @{message.from_user.username}
 
-<b>ǫᴜᴇʀʏ :</b> {message.text.split(None, 1)[1]}
+<b>⦿ 𝐒𝐞𝐚𝐫𝐜𝐡𝐞𝐝 ➺ :</b> {message.text.split(None, 1)[1]}
 <b>sᴛʀᴇᴀᴍᴛʏᴘᴇ :</b> {streamtype}"""
         if message.chat.id != LOGGER_ID:
             try:
