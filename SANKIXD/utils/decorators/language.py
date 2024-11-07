3beae319ce463b53b3dc9fbd0e7b266cfbd6fef5
𝐓𝐄𝐀𝐌 𝐒𝐀𝐍𝐊𝐈 <teamsanki001@gmail.com>
2024-11-07 20:43:40 +0530

    Update language.py

--- SANKIXD/utils/decorators/language.py
+++ SANKIXD/utils/decorators/language.py
@@ -1,5 +1,5 @@
-from DAXXMUSIC.misc import SUDOERS
-from DAXXMUSIC.utils.database import get_lang, is_maintenance
+from SANKIXD.misc import SUDOERS
+from SANKIXD.utils.database import get_lang, is_maintenance
 from strings import get_string
 
 
