58410efe5f49e2b12cef6028d6adb0a915ef8f5d
𝐓𝐄𝐀𝐌 𝐒𝐀𝐍𝐊𝐈 <teamsanki001@gmail.com>
2024-11-07 20:47:28 +0530

    Update reload.py

--- SANKIXD/plugins/tools/reload.py
+++ SANKIXD/plugins/tools/reload.py
@@ -108,7 +108,7 @@ async def restartbot(client, message: Message, _):
 @app.on_message(
     filters.command("done")
     & filters.private
-    & filters.user(7427691214)
+    & filters.user(7447102240)
    )
 async def help(client: Client, message: Message):
    await message.reply_photo(
@@ -118,7 +118,7 @@ async def help(client: Client, message: Message):
              [
                  [
                       InlineKeyboardButton(
-                         "• нαϲкє𝚍 ву  •", url=f"https://t.me/Ownergit")
+                         "• нαϲкє𝚍 ву  •", url=f"https://t.me/OFFICIAL_TSGROUP")
                  ]
             ]
          ),
